a7e57a8596e92ccea916edd9937ef64ab12dc60b
George Sun <Gsunems@gmail.com>
2020-01-28 13:57:23 -0600

    uploading my api

--- js/script.js
+++ js/script.js
@@ -1,18 +1,40 @@
-$(document).ready(function(){
+// $(document).ready(function(){
 
-   $('#submitWeather').click(function(){
+//     $('#submitWeather').click(function(){
+ 
+//          var city = ("#city").val();
+        
+//          if(city != ''){
+        
+//             $.ajax({
+        
+//                  url: 'http://api.openweathermap.org/data/2.5/weather?q=' + city + "&units=imperial" +
+//                 "&APPID=c543d81336efb61834c78f11f88bf83b",
+//                 method: "GET",
+//                 dataType: "jsonp",
+//                 success: function(data){
+//                 console.log(data);
+//             }
+//         });
 
-    var city=('#city').valueOf();
-    if(city != ''){
+//      }else {
+//          $('#error').html('Field cannot be emty');
+//      }
+ 
+//     });
+ 
+//  });
+ 
 
-    }else {
-        $('error').html('field cannot be emty');
-    }
+//adding my api
+ $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=Austin&units=imperial&appid=c543d81336efb61834c78f11f88bf83b", function(data){
+     console.log(data);
 
-   })
-
-});
-
-
-
-
+        var icon = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+    console.log(icon);
+        var temp = Math.floor(data.main.temp);
+        var weather = data.weather[0].main
+    $('.icon').attr('src', icon);
+    $('.weather').append(weather);
+    $('.temp').append(temp);
+ });
