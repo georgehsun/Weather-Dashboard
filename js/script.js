63df28cfbaf85997c1bde48d254acc087396c2b4
George Sun <Gsunems@gmail.com>
2020-01-30 16:14:57 -0600

    uploading my homework

--- js/script.js
+++ js/script.js
@@ -1,40 +1,76 @@
-// $(document).ready(function(){
+var cities = JSON.parse(localStorage.getItem('cities'))
+if (!cities) {
+    cities = []
+}
+var searchColumn = $("#searchColumn")
+var weatherColumn = $("#weatherColumn")
 
-//     $('#submitWeather').click(function(){
- 
-//          var city = ("#city").val();
-        
-//          if(city != ''){
-        
-//             $.ajax({
-        
-//                  url: 'http://api.openweathermap.org/data/2.5/weather?q=' + city + "&units=imperial" +
-//                 "&APPID=c543d81336efb61834c78f11f88bf83b",
-//                 method: "GET",
-//                 dataType: "jsonp",
-//                 success: function(data){
-//                 console.log(data);
-//             }
-//         });
+// cities.push('Denver')
+/* console.log(cities)
 
-//      }else {
-//          $('#error').html('Field cannot be emty');
-//      }
- 
-//     });
- 
-//  });
- 
+var myArr = ['Houston, Tx', 'Austin']
+console.log(myArr)
+console.log(myArr.toString())
+console.log(JSON.stringify(myArr))
 
-//adding my api
- $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=Austin&units=imperial&appid=c543d81336efb61834c78f11f88bf83b", function(data){
-     console.log(data);
+localStorage.setItem('cities', JSON.stringify(myArr)) */
+
+$(document).ready(function() {
+
+    if (cities.length > 0) {
+        searchColumn.removeClass('col-12').addClass('col-4')
+        weatherColumn.removeClass('hide').addClass('col-8')
+
+        displayCity(cities[0])
+    }
+
+    //     $('#submitWeather').click(function(){
+
+    //          var city = ("#city").val();
+
+    //          if(city != ''){
+
+    //             $.ajax({
+
+    //                  url: 'http://api.openweathermap.org/data/2.5/weather?q=' + city + "&units=imperial" +
+    //                 "&APPID=c543d81336efb61834c78f11f88bf83b",
+    //                 method: "GET",
+    //                 dataType: "jsonp",
+    //                 success: function(data){
+    //                 console.log(data);
+    //             }
+    //         });
+
+    //      }else {
+    //          $('#error').html('Field cannot be emty');
+    //      }
+
+    //     });
+
+    //  });
+
+
+
+
+    // if (navigator.geolocation){
+
+})
+
+function displayCity(city) {
+
+    //adding my api
+    $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=c543d81336efb61834c78f11f88bf83b", function(data) {
+        console.log(data);
 
         var icon = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
-    console.log(icon);
+        console.log(icon);
         var temp = Math.floor(data.main.temp);
-        var weather = data.weather[0].main
-    $('.icon').attr('src', icon);
-    $('.weather').append(weather);
-    $('.temp').append(temp);
- });
+        var humidity = Math.floor(data.main.humidity);
+        var windspeed = data.wind.speed
+        $('.icon').attr('src', icon);
+
+        $('.temp').append(temp + "ºF");
+        $('.humidity').append(humidity + "%");
+        $('.windspeed').append(windspeed + "MPH");
+    });
+}
