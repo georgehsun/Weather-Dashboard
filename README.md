0e6671467363a7b6a4f6703b9c71cae74bf26e60
George Sun <Gsunems@gmail.com>
2020-01-30 20:54:53 -0600

    uploading my read me

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Weather-Dashboard
+
+# Project Title
+Weather Dashboard
+## Getting Started
+This applicaiton will use an API to OpenWeatherMap.org to get and display Weather information for the City entered or chosen by the user.
+
+
+## Running the tests
+Choose a city. Type it where it says "city name". After that the weather should appear on the right side of the page.
+
+
+
+## Built With
+* bootstrap - The web framework used
+* font awesome 
+* jquery
+
--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
-var cities = JSON.parse(localStorage.getItem('cities'))
+// var cities = JSON.parse(localStorage.getItem('cities'))
+var cities = ["", ""];
+//push user's cities to this array
+//use a function to display this array to the page
+
 if (!cities) {
     cities = []
 }
@@ -16,38 +20,49 @@ console.log(JSON.stringify(myArr))
 localStorage.setItem('cities', JSON.stringify(myArr)) */
 
 $(document).ready(function() {
+    console.log(localStorage);
 
     if (cities.length > 0) {
         searchColumn.removeClass('col-12').addClass('col-4')
         weatherColumn.removeClass('hide').addClass('col-8')
 
-        displayCity(cities[0])
+        displayCity(cities[0]);
     }
 
-    //     $('#submitWeather').click(function(){
 
-    //          var city = ("#city").val();
 
-    //          if(city != ''){
+    $('#submitWeather').click(function() {
 
-    //             $.ajax({
+        $(".temp").empty();
+        $(".humidity").empty();
+        $(".windspeed").empty();
+        var userCity = $("#city").val();
+        console.log(userCity);
 
-    //                  url: 'http://api.openweathermap.org/data/2.5/weather?q=' + city + "&units=imperial" +
-    //                 "&APPID=c543d81336efb61834c78f11f88bf83b",
-    //                 method: "GET",
-    //                 dataType: "jsonp",
-    //                 success: function(data){
-    //                 console.log(data);
-    //             }
-    //         });
+        displayCity(userCity);
 
-    //      }else {
-    //          $('#error').html('Field cannot be emty');
-    //      }
 
-    //     });
 
-    //  });
+        // if (city != '') {
+
+        //     $.ajax({
+
+        //         url: 'http://api.openweathermap.org/data/2.5/weather?q=' + city + "&units=imperial" +
+        //             "&APPID=c543d81336efb61834c78f11f88bf83b",
+        //         method: "GET",
+        //         dataType: "jsonp",
+        //         success: function(data) {
+        //             console.log(data);
+        //         }
+        //     });
+
+        // } else {
+        //     $('#error').html('Field cannot be emty');
+        // }
+
+
+
+    });
 
 
 
@@ -61,16 +76,53 @@ function displayCity(city) {
     //adding my api
     $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=c543d81336efb61834c78f11f88bf83b", function(data) {
         console.log(data);
+        $(".city").html("");
 
         var icon = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
         console.log(icon);
         var temp = Math.floor(data.main.temp);
         var humidity = Math.floor(data.main.humidity);
         var windspeed = data.wind.speed
-        $('.icon').attr('src', icon);
 
-        $('.temp').append(temp + "ºF");
-        $('.humidity').append(humidity + "%");
-        $('.windspeed').append(windspeed + "MPH");
+        $('.icon').attr('src', icon);
+        $('.city').append("Location: " + city);
+        $('.temp').append("Temperature: " + temp + "ºF");
+        $('.humidity').append("Humidity: " + humidity + "%");
+        $('.windspeed').append("Windspeed: " + windspeed + "MPH");
     });
 }
+
+
+
+
+// function getFiveDay(city) {
+//     const dow = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+//     const APIKey = 'c543d81336efb61834c78f11f88bf83b';
+// }
+
+
+
+
+
+$(function() {
+    const history = JSON.parse(localStorage.getItem('searchHistory'));
+    if (history != null && history.length > 0) {
+        for (let i = 0; i < history.length; i++) {
+            const btn = $('<button>');
+            btn.addClass('w-100 text-left btnCity');
+            btn.text(history[i].cities);
+            btn.attr('onclick', 'cityButtonClicked(this)');
+            $('#searchList').append(btn);
+        }
+    }
+    $('.btn btn-primary').click(function() {
+        const city = $('#submitWeather').val();
+        const btn = $('<button>');
+        btn.addClass('w-100 text-left btnCity');
+        btn.text(city);
+        btn.attr('onclick', 'cityButtonClicked(this)');
+        $('#weatherColumn').append(btn);
+        saveSearchHistory(cities);
+        lkupCityWeather(cities);
+    });
+});
